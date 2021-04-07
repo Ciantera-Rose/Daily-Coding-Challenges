c3a43339ea47c6717a734ed78c0a91e938b5dffa
Ciantera-Rose <ciantera.rose@gmail.com>
2021-04-07 16:03:28 -0400

    add modify range function to include step

--- JS-Challenges/Sum-Of-A-Range.js
+++ JS-Challenges/Sum-Of-A-Range.js
@@ -7,17 +7,25 @@
     Next Write a sum function that takes an array of numbers and returns the sum of
     these numbers. 
 
+    Modify range function to take an optional third argument that indicates the "step" value used
+    when building the array. If no step is given, the elements go up by increments of one, 
+    corresponding to the old behavior. The function call range(1, 10, 2) should return [1,3,5,7,9]. 
+    Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+
 */
 
-function range(start, end) {
-  var array = [];
+function range(start, end, step = start <= end ? 1 : -1) {
+  let array = [];
 
-  for (let start = 0; start <= end; start++) {
-    array.push(start);
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) array.push(i);
+  } else {
+    for (let i = start; i >= end; i += step) array.push(i);
   }
   return array;
 }
-console.log(range(1, 10)); //? [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+console.log(range(1, 10));
 
 function sum(array) {
   var total = 0;
@@ -27,4 +35,4 @@ function sum(array) {
   }
   return total;
 }
-console.log(sum(range(1, 10))); //? 55
+console.log(sum(range(1, 10))); //?
